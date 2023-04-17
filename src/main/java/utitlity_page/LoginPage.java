eae0f1c66b10982aca88ece1225f474bc4fddfb0
Kamlesh Singh <121378322+kamleshisingh@users.noreply.github.com>
2023-04-17 18:52:36 +0530

    add constructor for loginPage

--- src/main/java/utitlity_page/LoginPage.java
+++ src/main/java/utitlity_page/LoginPage.java
@@ -1,10 +1,15 @@
 package utitlity_page;
 
+import org.openqa.selenium.support.PageFactory;
+
 import baselibrary.BaseLibrary;
 
 public class LoginPage extends	BaseLibrary
 {
-	
+	public LoginPage() 
+	{
+		PageFactory.initElements(driver, this);
+	}
 	public void getTitle() 
 	{
 	String	title =	driver.getTitle();
